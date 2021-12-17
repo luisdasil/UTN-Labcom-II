b1461c638435f569ebcb9fdded13dcc8d240a80a
luisdasil <90225199+luisdasil@users.noreply.github.com>
2021-12-16 21:46:07 -0300

    incomplete

--- TP-Luis Silva-CLIMA--API/JAVASCRIPT/index.js
+++ TP-Luis Silva-CLIMA--API/JAVASCRIPT/index.js
@@ -12,53 +12,57 @@ var card = document.getElementById("card")
 var buttonAddCity = document.getElementById("button-add-city")
 var cardError= document.getElementById("card-error")
 
-city = ["Rosario", "Brazilia", "NuevaYork", "Tokio"]
 
-function getWeather() {
-    api={
-        url:"api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=453fda7f094eec07c9612eedaac172e6&units=metric&lang=es"
-    }
-    fetch(`${api.url}`)
-        .then((cityStorage) => cityStorage.json())
-        .then(data => {
-        city.value = data['name'];
+
+// function getWeather() {
+//     api={
+//         url:"api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=453fda7f094eec07c9612eedaac172e6&units=metric&lang=es"
+//     }
+//     fetch(`${api.url}`)
+//         .then((response) => response.json())
+//         .then(data => {
+//         let temperature = data['main']['temp'];
+//         let thermalSensation = data['main']['feels_like'];
+//         let mainHumidity = data['main']['humidity'];
+//         let weatherDescription = data['weather'][0]['description']     
+
+//         main.innerHTML = city.value;
+//         temp.innerHTML =  temperature;
+//         desc.innerHTML = thermalSensation;
+//         clouds.innerHTML =  mainHumidity;
+//         weather.innerHTML = weatherDescription;
+//     })
+//     .catch(error => alert("error"))
+// }
+
+
+
+
+async function apiConection(city) {
+    try {
+        const response = await fetch(`api.openweathermap.org/data/2.5/weather?q="${city.value}&appid=453fda7f094eec07c9612eedaac172e6&units=metric&lang=es`)
+        const data = await response.json();
+        console.log(data)
         let temperature = data['main']['temp'];
         let thermalSensation = data['main']['feels_like'];
         let mainHumidity = data['main']['humidity'];
-        let weatherDescription = data['weather'][0]['description'];
+        let weatherDescription = data['weather'][0]['description']
 
-
-        
-        main.innerHTML = city.value
+        card.style.display= 'block'
+        main.innerHTML = city.value;
         temp.innerHTML =  temperature;
         desc.innerHTML = thermalSensation;
         clouds.innerHTML =  mainHumidity;
         weather.innerHTML = weatherDescription;
-    })
-    .catch(error => alert("error"))
-}
 
-
-
-
-async function apiConection() {
-    try {
-    var weather = await  getWeather();
-
-    let temperature = data['main']['temp'];
-    let thermalSensation = data['main']['feels_like'];
-    let mainHumidity = data['main']['humidity'];
-    let weatherDescription = data['weather'][0]['description'];
-
-
-    main.innerHTML = seleccion.value
-    temp.innerHTML =  temperature;
-    desc.innerHTML = thermalSensation;
-    clouds.innerHTML =  mainHumidity;
-    weather.innerHTML = weatherDescription;
 } catch (error) {
     console.log("error")
 }
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    apiConection(city.value)
 }
 
 
@@ -94,30 +98,18 @@ async function apiConection() {
 // }
 
 
-// function changeCity() {
-//     city.change(function() {
-//         localStorage.setItem('ejemplo', this.value);
-//     });
-//     if(localStorage.getItem('ejemplo')){
-//         $('#mi-select').val(localStorage.getItem('ejemplo'));
-//     }
-// };
-console.log(apiConection())
-console.log(getWeather())
-console.log(main)
 
+
+city = ["Rosario", "Brazilia", "NuevaYork", "Tokio"]
 
 function addNewCity(){
 
-    if(newCity.value != city.option || newCity.value != getWeather(data.name) ){
+
         localStorage.getItem(city.options)
 
-        localStorage.setItem(city.options.add(new Option(document.getElementById("addCity").value)))
+        localStorage.setItem(city.option.add(newCity.value))
 
         buttonAddCity.className += "animated__animated animate__fadeOutRightBig animate__delay-2s "
-    }
-    else{
-        card.className = "card-error";
-    }
+    
 
 }
